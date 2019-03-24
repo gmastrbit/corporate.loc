1d2b9984205471329cc5cb02c774107f4f5c4194
gmastrbit <gmastrbit@gmail.com>
2019-03-24 14:56:04 +0300

    add: created DB

---
+++ database/migrations/2019_03_24_112619_ChangeArticlesTable.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // зв'язування таблиці artiles з таблицею users
+        Schema::table('articles', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned()->default(1);
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->integer('category_id')->unsigned()->default(1);
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('articles');
+    }
+}
