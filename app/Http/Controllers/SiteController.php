7594d5978aa2e5539a08534105aac94a0af79d2a
gmastrbit <gmastrbit@gmail.com>
2019-03-27 13:54:34 +0300

    add: menu items

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -5,7 +5,10 @@ namespace Corp\Http\Controllers;
 use Illuminate\Http\Request;
 
 use Corp\Http\Requests;
+
 use Corp\Repositories\MenusRepository;
+
+use Menu;
 
 class SiteController extends Controller
 {
@@ -42,13 +45,25 @@ class SiteController extends Controller
 
     protected function renderOutput(){
         $menu = $this->getMenu();
-        $navigation = view(env('THEME').'.navigation')->render();
+        $navigation = view(env('THEME').'.navigation')->with('menu', $menu)->render();
         $this->vars = array_add($this->vars, 'navigation', $navigation);
         return view($this->template)->with($this->vars);
     }
 
     protected function getMenu(){
         $menu = $this->m_rep->get();
-        return $menu;
+        $mBuilder = Menu::make('MyNav', function($m) use ($menu){
+            // функція колбек додасть у меню необхідні пункти
+            foreach ($menu as $item) {
+                if ($item->parent == 0) {
+                    $m->add($item->title, $item->path)->id($item->id);
+                } else {
+                    if ($m->find($item->parent)) {
+                        $m->find($item->parent)->add($item->title, $item->path)->id($item->id);
+                    }
+                }
+            }
+        });
+        return $mBuilder;
     }
 }
